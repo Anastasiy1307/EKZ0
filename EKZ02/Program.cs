c0d1a9b742f50cd731106791a3418e89896e6141
Anastasia Vlasova <anastasiy131100714@gmail.com>
2022-09-20 13:50:03 +0300

    FFF

--- EKZ02/Program.cs
+++ EKZ02/Program.cs
@@ -12,5 +12,39 @@ namespace EKZ02
         {
 
         }
+        public static void Critical_Path(int n, int[] i, int[] j, int[] dij,
+                       int[] s1, int[] s2, int[] f1, int[] f2, int[] tf, int[] ff)
+        {
+            int k, index, max, min;
+            int[] ti = new int[20];
+            int[] te = new int[20];
+
+            index = 0;
+            for (k = 0; k < n; k++)
+            {
+                if (i[k] == index + 1) index = i[k];
+                ti[k] = 0; te[k] = 9999;
+            }
+
+            for (k = 0; k < n; k++)
+            {
+                max = ti[i[k]] + dij[k];
+                if (ti[j[k]] < max) ti[j[k]] = max;
+            }
+
+            te[j[n - 1]] = ti[j[n - 1]];
+            for (k = n - 1; k >= 0; k--)
+            {
+                min = te[j[k]] - dij[k];
+                if (te[i[k]] > min) te[i[k]] = min;
+            }
+
+            for (k = 0; k < n; k++)
+            {
+                s1[k] = ti[i[k]]; f1[k] = s1[k] + dij[k];
+                f2[k] = te[j[k]]; s2[k] = f2[k] - dij[k];
+                tf[k] = f2[k] - f1[k]; ff[k] = ti[j[k]] - f1[k];
+            }
+        }
     }
 }
